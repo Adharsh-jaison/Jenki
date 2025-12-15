54fde1935f3ae8986703a05f57046a0d452b35e4
Adharsh-jaison <jaisonadharsh87@gmail.com>
2025-12-15 09:22:43 +0530

    py updated z

--- hello.py
+++ hello.py
@@ -10,7 +10,13 @@ print("Starting the calculation...")
 num1 = 10
 num2 = 20
 
+def sumz(a,b):
+    sum = a+z
+    return sum
+
+summ = sumz(num1, num2)
+
 
 average = calculate_average(num1, num2)
 
-print(f"The average is: {average}")
+print(f"The average is: {average} and sum is {summ}")
