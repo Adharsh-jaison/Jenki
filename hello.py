7d1a03faa04e901a7bc2a39461c26c1141419291
Adharsh-jaison <jaisonadharsh87@gmail.com>
2025-12-15 08:56:25 +0530

    py updated

--- hello.py
+++ hello.py
@@ -1,3 +1,16 @@
-print("Hello, Jenkins!")
-print("This code has an error) 
-prnt("Testing Automation")
+
+def calculate_average(a, b):
+    
+    result = (a + c) / 2 
+    return result
+
+
+print("Starting the calculation...")
+
+num1 = 10
+num2 = 20
+
+
+average = calculate_average(num1, num2)
+
+print(f"The average is: {average}")
